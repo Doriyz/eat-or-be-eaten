b68356ec8e01782b1db628577d208ab80ff81863
MaysionZhang <319474628@qq.com>
2023-03-14 18:30:28 +0800

    add game window

--- src/App.js
+++ src/App.js
@@ -3,22 +3,51 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div id="game-container">
+      <div class="header">
+        <h1>EAT OR BE EATEN</h1>
+      </div>
+      
+      <div id="game-window">
+        <div id="game-content"> 
+          <p>hello</p>
+        </div>
+      </div>
+
+      
+
+      <div class="footer">
+        <p>Made by Maysion</p>
+      </div>
     </div>
+
+
+
+    // <div className="App">
+
+      
+
+    //   {/* fellow will be discarded */}
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+
+
+
   );
 }
 
